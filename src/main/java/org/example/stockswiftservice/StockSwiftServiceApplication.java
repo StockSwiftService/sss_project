3c89dd5e8962b17a1a2d92867c04d403f6b7f055
ckh <gusrudchl12@gmail.com>
2024-02-13 11:29:45 +0900

    first commit

---
+++ src/main/java/org/example/stockswiftservice/StockSwiftServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.stockswiftservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockSwiftServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockSwiftServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/stockswiftservice/StockSwiftServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.stockswiftservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockSwiftServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
