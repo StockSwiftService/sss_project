4c04c65d0021ad45ac85e1ed016aa0b2b937062c
kkh <k2ho33333@gmail.com>
2024-02-14 13:40:36 +0900

    학원에서 푸시

---
+++ frontapp/src/components/LeftMenu.svelte
@@ -0,0 +1,74 @@
+<style>
+.logo-box {
+    width: 120px;
+}
+</style>
+
+<div class="logo-box img-box mgc">
+    <img src="../img/logo.svg" alt="">
+</div>
+<ul class="menu-box">
+    <li>
+        <a href="../sub/member-company-management.html" class="flex aic">
+            <span class="ico-box img-box">
+                <img src="../img/menu_ico_2.svg" alt="메뉴 아이콘">
+                <img src="../img/menu_ico_2_active.svg" alt="메뉴 액티브 아이콘" class="on">
+            </span>    
+            <span class="text tm">회원 관리</span>
+        </a>
+    </li>
+    <li>
+        <a href="../sub/dashboard.html" class="flex aic">
+            <span class="ico-box img-box">
+                <img src="../img/menu_ico_7.svg" alt="메뉴 아이콘">
+                <img src="../img/menu_ico_7_active.svg" alt="메뉴 액티브 아이콘" class="on">
+            </span>    
+            <span class="text tm">거래처 관리</span>
+        </a>
+    </li>
+    <li>
+        <a href="#none" class="flex aic">
+            <span class="ico-box img-box">
+                <img src="../img/menu_ico_3.svg" alt="메뉴 아이콘">
+                <img src="../img/menu_ico_3_active.svg" alt="메뉴 액티브 아이콘" class="on">
+            </span>    
+            <span class="text tm">재고 관리</span>
+        </a>
+    </li>
+    <li>
+        <a href="../sub/member-company-management.html" class="flex aic">
+            <span class="ico-box img-box">
+                <img src="../img/menu_ico_5.svg" alt="메뉴 아이콘">
+                <img src="../img/menu_ico_5_active.svg" alt="메뉴 액티브 아이콘" class="on">
+            </span>    
+            <span class="text tm">판매 관리</span>
+        </a>
+    </li>
+    <li>
+        <a href="../sub/store-management.html" class="flex aic">
+            <span class="ico-box img-box">
+                <img src="../img/menu_ico_13.svg" alt="메뉴 아이콘">
+                <img src="../img/menu_ico_13_active.svg" alt="메뉴 액티브 아이콘" class="on">
+            </span>    
+            <span class="text tm">구매 관리</span>
+        </a>
+    </li>
+    <li>
+        <a href="#none" class="flex aic">
+            <span class="ico-box img-box">
+                <img src="../img/menu_ico_11.svg" alt="메뉴 아이콘">
+                <img src="../img/menu_ico_11_active.svg" alt="메뉴 액티브 아이콘" class="on">
+            </span>    
+            <span class="text tm">매출 관리</span>
+        </a>
+    </li>
+    <li>
+        <a href="#none" class="flex aic">
+            <span class="ico-box img-box">
+                <img src="../img/menu_ico_6.svg" alt="메뉴 아이콘">
+                <img src="../img/menu_ico_6_active.svg" alt="메뉴 액티브 아이콘" class="on">
+            </span>    
+            <span class="text tm">자주 묻는 질문</span>
+        </a>
+    </li>
+</ul>
---
+++ frontapp/src/components/LeftMenuAdmin.svelte
@@ -0,0 +1,29 @@
+<style>
+.logo-box {
+    width: 120px;
+}
+</style>
+
+<div class="logo-box img-box mgc">
+    <img src="../img/logo.svg" alt="">
+</div>
+<ul class="menu-box">
+    <li>
+        <a href="../sub/member-company-management.html" class="flex aic">
+            <span class="ico-box img-box">
+                <img src="../img/menu_ico_7.svg" alt="메뉴 아이콘">
+                <img src="../img/menu_ico_7_active.svg" alt="메뉴 액티브 아이콘" class="on">
+            </span>    
+            <span class="text tm">회사 관리</span>
+        </a>
+    </li>
+    <li>
+        <a href="../sub/dashboard.html" class="flex aic">
+            <span class="ico-box img-box">
+                <img src="../img/menu_ico_6.svg" alt="메뉴 아이콘">
+                <img src="../img/menu_ico_6_active.svg" alt="메뉴 액티브 아이콘" class="on">
+            </span>    
+            <span class="text tm">질문 관리</span>
+        </a>
+    </li>
+</ul>
