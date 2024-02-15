2b19c442bfd54282de403fc506205e5c0c19ad3d
hwjk16322@gmail.com <hwjk16322@gmail.com>
2024-02-15 09:01:44 +0900

    쿠키에 토큰넣음

--- src/main/java/org/example/stockswiftservice/domain/member/service/MemberService.java
+++ src/main/java/org/example/stockswiftservice/domain/member/service/MemberService.java
@@ -2,30 +2,82 @@ package org.example.stockswiftservice.domain.member.service;
 
 
 import lombok.RequiredArgsConstructor;
+import org.example.stockswiftservice.domain.company.entity.Company;
+import org.example.stockswiftservice.domain.company.repository.CompanyRepository;
 import org.example.stockswiftservice.domain.member.entity.Member;
 import org.example.stockswiftservice.domain.member.repository.MemberRepository;
+import org.example.stockswiftservice.global.jwt.JwtProvider;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
+    private final CompanyRepository companyRepository;
 
 
-
-    public void repJoin(String repName, String username, String password, LocalDate birthday) {
+    public void repJoin(String repName, String username, String password, LocalDate birthday, Company companyCode) {
         Member rep = Member.builder()
                 .name(repName)
-                .rank("대표이사")
+                .position("대표이사")
                 .username(username)
                 .password(passwordEncoder.encode(password))
                 .birthday(birthday)
+                .company(companyCode)
                 .authority(2)
                 .build();
         this.memberRepository.save(rep);
+    }
+
+    public String genAccessToken(String username, String companyCode) {
+        Member member = findByUsernameAndCompanyCode(username,companyCode).orElse(null);
+
+        if (member == null) return null;
+
+
+
+        return jwtProvider.genToken(member.toClaims(), 60 * 5);
+    }
+
+    private Optional<Member> findByUsernameAndCompanyCode(String username,String companyCode) {
+        Company optionalCompany = this.companyRepository.findByCompanyCode(companyCode).orElse(null);
+        return this.memberRepository.findByUsernameAndCompany(username,optionalCompany);
+    }
+
+    private Optional<Member> findByUsername(String username) {
+        return this.memberRepository.findByUsername(username);
+    }
+
+    public String genRefreshToken(String username, String companyCode) {
+        Member member = findByUsernameAndCompanyCode(username,companyCode).orElse(null);
+
+        if (member == null) return null;
+
+        return jwtProvider.genToken(member.toClaims(), 60 * 60 * 24 * 365);
+    }
+
+    public String genNewAccessToken(String username) {
+        Member member = findByUsername(username).orElse(null);
+
+        if (member == null) return null;
+
+        return jwtProvider.genToken(member.toClaims(), 60 * 5);
+    }
+
+    public boolean userCheck(String companyCode, String username, String password) {
+        Optional<Member> optionalMember = findByUsernameAndCompanyCode(username, companyCode);
+        Member searchMember = optionalMember.orElse(null);
+
+        if (searchMember != null) {
+            return passwordEncoder.matches(password, searchMember.getPassword());
+        } else {
+            return false;
+        }
     }
 }
